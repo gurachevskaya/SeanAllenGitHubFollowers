53367d04780887aaaadd5702f1ca71973cb31218
Karina Gurachevskaya <k.gurachevskaya@gmail.com>
2020-10-13 11:25:08 +0300

    feat: add collectionViewCell, configure collectionViewLayout

---
+++ SHCourse/Custom Views/KGAvatarImageView.swift
@@ -0,0 +1,33 @@
+//
+//  KGAvatarImageView.swift
+//  SHCourse
+//
+//  Created by Karina on 10/13/20.
+//  Copyright © 2020 Karina. All rights reserved.
+//
+
+import UIKit
+
+class KGAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius  = 10
+        //check
+        clipsToBounds       = true
+        image               = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
--- SHCourse/Screens/FollowerListVC.swift
+++ SHCourse/Screens/FollowerListVC.swift
@@ -11,22 +11,13 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            
-            switch result {
-            case .success(let followers):
-               print("Followers.count = \(followers.count)")
-               print(followers)
-            case .failure(let error):
-                 self.presentKGAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
-            }
-        }
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
     }
     
     
@@ -35,4 +26,46 @@ class FollowerListVC: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
+    
+    private func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    
+    private func getFollowers() {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print("Followers.count = \(followers.count)")
+                print(followers)
+            case .failure(let error):
+                self.presentKGAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
 }
